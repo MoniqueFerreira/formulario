c65a6c1273e4760b3f74819d0ee78363bf8aa10a
MoniqueFerreira <42048828+MoniqueFerreira@users.noreply.github.com>
2018-09-15 00:44:01 -0300

    finalizado

---
+++ js/arq-js.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+  $('.date').mask('00/00/0000');
+  $('.CEP').mask('00000-000');
+  $('.Telefone').mask('(00) 0000-0000');
+  $('.CPF').mask('000.000.000-00', {reverse: true});
+
+});
+
